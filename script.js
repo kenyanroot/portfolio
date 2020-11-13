360eb34f53f14983ff57be6396086b700896e3ad
kenyanroot <46379959+kenyanroot@users.noreply.github.com>
2020-11-13 13:56:33 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+
+
+
+
+function createbubble(){
+    const section = document.querySelector('section')
+    const createElement = document.createElement('span')
+    var size = Math.random() * 60;
+    
+    createElement.style.width = 20 + size + 'px';
+    createElement.style.height = 20 + size + 'px';
+    createElement.style.left = Math.random() * innerWidth + "px";
+    section.appendChild(createElement);
+    
+    setTimeout(() => {
+        createElement.remove()
+    },4000)
+
+}
+
+setInterval(createbubble, 50)
+
+
+
